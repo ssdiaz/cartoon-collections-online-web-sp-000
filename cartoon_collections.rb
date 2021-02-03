c3bd060a94002af9e991b9ad679d4fd199ca464e
Sam D <samidiaz3@gmail.com>
2021-02-03 03:10:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,5 @@ end
 
 def find_the_cheese(strings)
    cheese_types = ["cheddar", "gouda", "camembert"]
-   #strings.find { |item| item.include?(cheese_types[item])}
    strings.find { |item| cheese_types.include?(item)}
 end
