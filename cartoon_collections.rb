1be647eb6925766909b68646cce04c27bc32aaf0
Sam D <samidiaz3@gmail.com>
2021-02-03 01:58:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index { |name| puts name}
+  name += 1
 end
 
 def summon_captain_planet# code an argument here
